1ddb88df07186a43b5aae515e5f36c12f3f203f2
smartfix <laughtermayour99@gmail.com>
2022-11-19 15:47:00 -0800

    the troll and the flea madlibz game

---
+++ main.py
@@ -0,0 +1,59 @@
+
+print("fill in the gap with the most closest answer to the the story",
+"pretzel crayon clown flea squeaked blasted fainted slimed trick flip flush freeze stinky sweaty hunky tiny"
+)
+
+print("LETS GO....", "pretzel crayon clown flea squeaked blasted fainted slimed trick flip flush freeze stinky sweaty hunky tiny")
+
+option1 = input("One day, a ......1...... wanted to cross a bridge over a pond.")
+option2 = input("Under that bridge lived a....2....")
+option10 = input("and......3")
+option3 = input("troll. Stamp, stomp, stamp. He....4....... over the bridge.")
+show1 = input("“Who tramps over my bridge?” yelled the troll. “Stop or I will")
+option4 = input("....5...you!” But he had already crossed the bridge.")
+option5 = input("On the next day, a sleepy ....6..... wanted to cross the bridge")
+option6 = input("Stamp, stomp, stamp. He...7... over the bridge.")
+show2 =input("“Who tramps over my bridge?” yelled the troll. “Stop or")
+option7 = input("I will....8.... you! Oh, never mind.")
+show3 = input("Nobody listens to me anyway!” And with that,")
+option8 = input("the troll away...9...")
+
+print(f"One day, a {option1} wanted to cross a bridge over a pond.",
+f"Under that bridge lived a {option2} and {option10}",
+f"troll. Stamp, stomp, stamp. He {option3} over the bridge."
+f"“Who tramps over my bridge?” yelled the troll. “Stop or I will {option4} you!”",
+" But he had already crossed the bridge.",
+f"On the next day, a sleepy {option5} wanted to cross the bridge",
+f"Stamp, stomp, stamp. He {option6} over the bridge.",
+"“Who tramps over my bridge?” yelled the troll. “Stop or"
+f"I will {option7} you! Oh, never mind."
+"Nobody listens to me anyway!” And with that,"
+f"the troll {option8}away")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#One day, a wanted to cross a bridge over a pond.
+#Under that bridge lived a and
+#troll. Stamp, stomp, stamp. He over the bridge.
+#“Who tramps over my bridge?” yelled the troll. “Stop or I will
+# you!” But he had already crossed the bridge.
+#On the next day, a sleepy wanted to cross the bridge
+#. Stamp, stomp, stamp. He over the bridge.
+#“Who tramps over my bridge?” yelled the troll. “Stop or
+#I will you! Oh, never mind.
+#Nobody listens to me anyway!” And with that,
+#the troll away
